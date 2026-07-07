f3613f53b2a2fd169d988e83778e8245072567bb
test1 <test1>
2026-07-07 09:18:13 +0800

    1

---
+++ assets/scripts/LeagueAnalysis/DateSelect.js
@@ -0,0 +1,10 @@
+var LeagueAnalysisApi = require("./LeagueAnalysisApi");
+
+cc.Class({
+    extends: cc.Component,
+    properties:{
+    },
+    init:function(){
+       
+    }
+});
---
+++ assets/scripts/LeagueAnalysis/PaginationNode.js
@@ -0,0 +1,16 @@
+cc.Class({
+    extends: cc.Component,
+    properties: {},
+    onLoad: function () {
+    },
+    setPage: function (page, totalPage) {
+        
+    },
+    setEnabled(prevEnabled, nextEnabled) {
+        
+    },
+    onPrevClick() {
+        
+    },
+    onNextClick(){}
+});
